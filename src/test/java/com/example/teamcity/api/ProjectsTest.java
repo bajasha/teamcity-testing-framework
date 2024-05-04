190f8d144f33757adad9ebef41c34f544cde86b0
bajasha <seretyrov@gmail.com>
2024-05-04 23:42:54 +0300

    add project creation negative tests

--- src/test/java/com/example/teamcity/api/ProjectsTest.java
+++ src/test/java/com/example/teamcity/api/ProjectsTest.java
@@ -3,7 +3,6 @@ package com.example.teamcity.api;
 import com.example.teamcity.api.generators.RandomData;
 import com.example.teamcity.api.requests.unchecked.UncheckedProject;
 import com.example.teamcity.api.spec.Specifications;
-import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
@@ -11,16 +10,15 @@ import org.testng.annotations.Test;
 import static java.lang.String.format;
 
 public class ProjectsTest extends BaseApiTest {
+
 @Test
 public void userUnableToCreateTwoProjectsWithSameId() {
     var testData = testDataStorage.addTestData();
 
-    //testData.getUser().setRoles(TestDataGenerator.generateRoles(Role.SYSTEM_ADMIN, "g"));
-
     checkedWithSuperUser.getUserRequest()
             .create(testData.getUser());
 
-    var projectOne = new UncheckedProject(Specifications.getSpec()
+    new UncheckedProject(Specifications.getSpec()
             .authSpec(testData.getUser()))
             .create(testData.getProject());
 
@@ -32,4 +30,24 @@ public void userUnableToCreateTwoProjectsWithSameId() {
             .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
             .body(Matchers.containsString(format("Project ID \"%s\" is already used by another project", testData.getProject().getId())));
 }
+
+    @Test
+    public void userUnableToCreateTwoProjectsWithSameName() {
+        var testData = testDataStorage.addTestData();
+
+        checkedWithSuperUser.getUserRequest()
+                .create(testData.getUser());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        testData.getProject().setId(RandomData.getString());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString(format("Project with this name already exists: %s", testData.getProject().getName())));
+    }
 }
